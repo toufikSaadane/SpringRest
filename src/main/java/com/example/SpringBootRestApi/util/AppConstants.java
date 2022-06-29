588753706eaa446be8ebd9d8f01355867fdb7aaf
taoufikSaadane <toufik.saadane@gmail.com>
2022-06-29 12:17:29 +0200

    properties

--- src/main/java/com/example/SpringBootRestApi/util/AppConstants.java
+++ src/main/java/com/example/SpringBootRestApi/util/AppConstants.java
@@ -2,6 +2,6 @@ package com.example.SpringBootRestApi.util;
 
 public class AppConstants {
   public static final String DEFAULT_PAGE_NUMBER = "0";
-  public static final String DEFAULT_PAGE_SIZE = "7";
+  public static final String DEFAULT_PAGE_SIZE = "170";
   public static final String DEFAULT_PAGE_SORT_PARAM = "id";
 }
